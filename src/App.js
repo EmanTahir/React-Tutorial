00455514fdd6e8dcadfc27c8f85e3111fd678f05
EmanTahir <emantahir555@example.com>
2022-08-15 17:51:57 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+let name = "Eman"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="container">
+        <h1>Hello {name}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, 
+          perferendis. Et dolore provident unde voluptatem repudiandae, vitae 
+          libero autem molestiae, numquam doloremque, commodi dicta? Labore veniam 
+          ratione debitis tempore beatae?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
