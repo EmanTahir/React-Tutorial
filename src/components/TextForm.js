1cb4fd4cca364299e515705ff7537832377f5209
EmanTahir <emantahir555@example.com>
2022-08-16 16:19:51 +0500

    Change text to lowercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,19 +6,33 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick = ()=>{
+        console.log("Lower was clicked");
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
     const handleOnChange = (event)=>{
         console.log("OnChange");
         setText(event.target.value)
     }
-    const[text, setText] = useState('Enter your text here');
-return (
+    const[text, setText] = useState('');
+
+    return (
     <>
     <h1>{props.heading}</h1>
     <div className="mb-3">
         <label htmlFor="myBox" className="form-label">  </label>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}> Convert to Uppercase </button>
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to Uppercase </button>
+    <button className="btn btn-primary" onClick={handleLoClick}> Convert to Lowercase </button>
+    <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} character(s)</p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
     </>
   )
 }
