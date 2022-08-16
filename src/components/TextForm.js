6d0cfbcae84a211facfe0156b5cb8a96c1552f81
EmanTahir <emantahir555@example.com>
2022-08-16 16:25:54 +0500

    Clear text button added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        console.log("Uppercase was clicked");
         let newText = text.toUpperCase();
         setText(newText)
     }
     const handleLoClick = ()=>{
-        console.log("Lower was clicked");
         let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClClick = ()=>{
+        let newText = '';
         setText(newText)
     }
     const handleOnChange = (event)=>{
@@ -24,8 +26,9 @@ export default function TextForm(props) {
         <label htmlFor="myBox" className="form-label">  </label>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary mx-2" onClick={handleUpClick}> Convert to Uppercase </button>
-    <button className="btn btn-primary" onClick={handleLoClick}> Convert to Lowercase </button>
+    <button className="btn btn-primary mx-1" onClick={handleUpClick}> Convert to Uppercase </button>
+    <button className="btn btn-primary mx-1" onClick={handleLoClick}> Convert to Lowercase </button>
+    <button className="btn btn-primary mx-1" onClick={handleClClick}> Clear </button>
     <div className="container my-3">
         <h2>Your Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} character(s)</p>
